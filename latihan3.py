c92621ad99e9683d833d94517a787e4e98aa7ad1
muhammadabdulanas <anasbufalowlery@gmail.com>
2022-10-19 11:27:54 +0700

    ddk

---
+++ latihan3.py
@@ -0,0 +1,16 @@
+# input nilai variabel
+a=input("masukan nilai a:")
+b=input("masukan nilai b:")
+
+# cetak nilai variabel
+print("Variabel a=",a)
+print("Variabel b=",b)
+
+# cetak hasil operasi kedua variabel dengan String Format
+print("Hasil penggabung (1)&(0)=%d".format(a,b) %(a+b) 
+
+# konversi nilai variabel
+a=int(a)
+b=int(b)
+print("Hasil penjumlahan {1}&{0}=%d".format(a,b) %(a+b)
+print("Hasil pembagian {1}/{0}=%d".format(a,/b))
